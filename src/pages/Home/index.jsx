0cda07811d5f1ad42f146249639b36d6377fe16c
Fernando Carmona <fernando.carayu@gmail.com>
2021-05-31 18:53:07 +0200

    small changes¡

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1>Bienvenid@ a The Bridge Auth</h1>;
+  return <h1>Coches eléctricos</h1>;
 };
 
 export default Home;
---
+++ src/services/reservations.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+import { API_URL } from "../constants";
+
+export const postStartReservation = (token, connectionId) => {};
