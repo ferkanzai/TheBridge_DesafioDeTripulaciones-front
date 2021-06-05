54af6f8bd5fdf7fd0362daa7e8988335be4ed324
Fernando Carmona <fernando.carayu@gmail.com>
2021-06-05 17:33:30 +0200

    redirect to start if user

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,37 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Redirect } from "react-router";
+
+import { UserContext } from "../../store";
 
 import "./index.scss";
 
 const Home = () => {
+  const { user } = useContext(UserContext);
+
   return (
-    <div className="home">
-      <div className="home__content">
-        <h1 className="home__content__title">
-          Viaja largos trayectos con tu vehículo <br /> eléctrico
-        </h1>
-        <Link to="/signup" className="home__content__signup">
-          Crea una cuenta
-        </Link>
-        <Link to="/login" className="home__content__login">
-          Inicia sesión
-        </Link>
-        <Link to="/map" className="home__content__continue">
-          Sólo quiero echar un vistazo
-        </Link>
-      </div>
-    </div>
+    <>
+      {!user ? (
+        <div className="home">
+          <div className="home__content">
+            <h1 className="home__content__title">
+              Viaja largos trayectos con tu vehículo eléctrico
+            </h1>
+            <Link to="/signup" className="home__content__signup">
+              Crea una cuenta
+            </Link>
+            <Link to="/login" className="home__content__login">
+              Inicia sesión
+            </Link>
+            <Link to="/map" className="home__content__continue">
+              Sólo quiero echar un vistazo
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <Redirect to="/start" />
+      )}
+    </>
   );
 };
 
