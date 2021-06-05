3c6290f70d2b5e8668b66059773e9248a47f18c8
Fernando Carmona <fernando.carayu@gmail.com>
2021-06-05 17:33:49 +0200

    formatting

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,17 +1,27 @@
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
-const Form = ({message = 'Subimt form', handleFormSubmit}) => {
+const Form = ({ message = "Subimt form", handleFormSubmit }) => {
   const { handleSubmit, register } = useForm();
 
   return (
     <form onSubmit={handleSubmit(handleFormSubmit)}>
-      <label htmlFor='email'>Email</label>
-      <input type='email' id='email' name='email' {...register('email', { required: true })} />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        {...register("email", { required: true })}
+      />
       <br />
-      <label htmlFor='password'>Password</label>
-      <input type='password' id='password' name='password' {...register('password', { required: true })} />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        {...register("password", { required: true })}
+      />
       <br />
-      <button type='submit'>{message}</button>
+      <button type="submit">{message}</button>
     </form>
   );
 };
