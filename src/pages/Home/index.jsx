ba5780670c7adc90d5ce5b2cf62f986dcf6c313f
Fernando Carmona <fernando.carayu@gmail.com>
2021-06-05 10:42:27 +0200

    home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+
+import "./index.scss";
+
 const Home = () => {
-  return <h1>Coches eléctricos</h1>;
+  return (
+    <div className="home">
+      <div className="home__content">
+        <h1 className="home__content__title">
+          Viaja largos trayectos con tu vehículo <br /> eléctrico
+        </h1>
+        <Link to="/signup" className="home__content__signup">
+          Crea una cuenta
+        </Link>
+        <Link to="/login" className="home__content__login">
+          Inicia sesión
+        </Link>
+        <Link to="/map" className="home__content__continue">
+          Sólo quiero echar un vistazo
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
