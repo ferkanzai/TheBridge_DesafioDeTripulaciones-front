a9c36e0b5070c951627a7f738fa2a2338ec37bd8
Fernando Carmona <fernando.carayu@gmail.com>
2021-06-05 17:46:19 +0200

    get past reservations

--- src/services/reservations.js
+++ src/services/reservations.js
@@ -2,3 +2,16 @@ import axios from "axios";
 import { API_URL } from "../constants";
 
 export const postStartReservation = (token, connectionId) => {};
+
+export const getUserReservations = async (token) => {
+  const config = {
+    headers: {
+      authorization: token,
+    },
+    withCredentials: true,
+  };
+
+  const response = await axios.get(`${API_URL}/reservations/past`, config);
+
+  return response.data.data;
+};
