b9f84997cf4c40c4bef46ce166ca6984aac30d23
Fernando Carmona <fernando.carayu@gmail.com>
2021-06-10 00:55:20 +0200

    add user car component styles

--- src/components/AddUserCar/index.jsx
+++ src/components/AddUserCar/index.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useHistory } from "react-router";
+
 import { getBrands, getCarsByBrand } from "../../services/brands";
+
+import "./index.scss";
 
 const AddUserCar = ({ message = "Subimt form", handleFormSubmit }) => {
   const { handleSubmit, register, reset } = useForm();
   const [brands, setBrands] = useState([]);
   const [brand, setBrand] = useState("");
   const [cars, setCars] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     getBrands().then((res) => {
@@ -22,39 +27,50 @@ const AddUserCar = ({ message = "Subimt form", handleFormSubmit }) => {
   }, [brand]);
 
   return (
-    <form onSubmit={handleSubmit((v) => handleFormSubmit(v, reset))}>
-      <label htmlFor="Brand">Brand</label>
-      <select
-        id="brand"
-        name="brand"
-        {...register("brand", { required: true })}
-        onChange={setBrand}
-      >
-        <option value="0" defaultValue>
-          Select a brand
-        </option>
-        {brands.length &&
-          brands.map((brand) => (
-            <option key={brand.id} value={brand.id}>
-              {brand.name}
-            </option>
-          ))}
-      </select>
-      <br />
-      <label htmlFor="Model">Model</label>
-      <select id="car" name="car" {...register("car", { required: true })}>
-        <option value="" defaultValue>
-          Select a car
-        </option>
-        {cars.length &&
-          cars.map((car) => (
-            <option key={car.id} value={car.id}>
-              {car.model}
-            </option>
-          ))}
-      </select>
-      <br />
-      <button type="submit">{message}</button>
+    <form
+      onSubmit={handleSubmit((v) => handleFormSubmit(v, reset))}
+      className="addUserCar"
+    >
+      <div className="addUserCar__selects">
+        <select
+          id="brand"
+          name="brand"
+          {...register("brand", { required: true })}
+          onChange={setBrand}
+        >
+          <option value="0" defaultValue>
+            Marca
+          </option>
+          {brands.length &&
+            brands.map((brand) => (
+              <option key={brand.id} value={brand.id}>
+                {brand.name}
+              </option>
+            ))}
+        </select>
+        <select id="car" name="car" {...register("car", { required: true })}>
+          <option value="" defaultValue>
+            Modelo
+          </option>
+          {cars.length &&
+            cars.map((car) => (
+              <option key={car.id} value={car.id}>
+                {car.model}
+              </option>
+            ))}
+        </select>
+      </div>
+      <div className="addUserCar__buttons">
+        <button type="submit" className="addUserCar__buttons__add">
+          {message}
+        </button>
+        <button
+          className="addUserCar__buttons__later"
+          onClick={() => history.push("/profile")}
+        >
+          Más tarde
+        </button>
+      </div>
     </form>
   );
 };
