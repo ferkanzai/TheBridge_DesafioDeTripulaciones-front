21f2a23dc786db29e947aff6e979e452216c89f6
Fernando Carmona <fernando.carayu@gmail.com>
2021-06-11 13:10:57 +0200

    add mock search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 import exit from "../../svg/exit.svg";
 
 import "./index.scss";
 
 const Search = ({ toggleSearch }) => {
+  const [showResult, setShowResult] = useState(false);
+
+  const handleClick = () => {
+    setShowResult(!showResult);
+  };
+
   return (
     <div className="search">
       <div className="search__all">
@@ -15,7 +23,7 @@ const Search = ({ toggleSearch }) => {
             src={exit}
           />
         </div>
-        <form className="search__all__form">
+        <div className="search__all__form">
           <input
             type="text"
             placeholder="Origen"
@@ -26,13 +34,55 @@ const Search = ({ toggleSearch }) => {
             placeholder="Destino"
             className="search__all__form__input"
           />
-          <input
-            type="submit"
-            value="Buscar"
-            className="search__all__form__submit"
-          />
-        </form>
+          <button className="search__all__form__submit" onClick={handleClick}>
+            Buscar
+          </button>
+        </div>
       </div>
+      {showResult && (
+        <div className="search__result">
+          <div className="search__result__info">
+            <div className="search__result__info__points">
+              <div className="search__result__info__points__start">
+                Origen: Arturo Soria Plaza
+              </div>
+              <div className="search__result__info__points__end">
+                Destino: Centro Comercial Torre Sevilla
+              </div>
+            </div>
+            <div className="search__result__info__chargePoints">
+              Paradas recomendadas:
+              <div className="search__result__info__chargePoints__first">
+                Almaraz Supercharge
+              </div>
+              <div className="search__result__info__chargePoints__second">
+                Complejo Leo
+              </div>
+            </div>
+            Distancia total: <strong>539 km</strong>
+          </div>
+          <div className="search__result__info">
+            <div className="search__result__info__points">
+              <div className="search__result__info__points__start">
+                Origen: Arturo Soria Plaza
+              </div>
+              <div className="search__result__info__points__end">
+                Destino: Centro Comercial Torre Sevilla
+              </div>
+            </div>
+            <div className="search__result__info__chargePoints">
+              Paradas recomendadas:
+              <div className="search__result__info__chargePoints__first">
+                Carrefour Talavera de la Reina
+              </div>
+              <div className="search__result__info__chargePoints__second">
+                Parking Atarazanas
+              </div>
+            </div>
+            Distancia total: <strong>562 km</strong>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
