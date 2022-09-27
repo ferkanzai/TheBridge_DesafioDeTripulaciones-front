a22417a866045d65658b5e5f84cc035834421ce1
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2022-09-28 01:56:38 +0200

    fix: change API_URL to support render instead of heroku

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
-export const API_URL = `https://tripulaciones.herokuapp.com/api`;
+export const API_URL = `https://desafio-tripulaciones-back.onrender.com`;
+// export const API_URL = `https://tripulaciones.herokuapp.com/api`;
 // export const API_URL = `http://localhost:4000/api`;
